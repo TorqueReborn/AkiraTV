041592e348761e642979b1b23a1866f80aeaf537
TorqueReborn <torquereborn@gmail.com>
2024-08-18 19:05:18 +0530

    Enable Voice Search

--- app/src/main/java/com/ghostreborn/akiratv/fragment/SearchFragment.kt
+++ app/src/main/java/com/ghostreborn/akiratv/fragment/SearchFragment.kt
@@ -1,6 +1,15 @@
 package com.ghostreborn.akiratv.fragment
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.leanback.app.SearchSupportFragment
 import androidx.leanback.widget.ArrayObjectAdapter
 import androidx.leanback.widget.HeaderItem
@@ -17,11 +26,18 @@ import kotlinx.coroutines.withContext
 class SearchFragment : SearchSupportFragment(), SearchSupportFragment.SearchResultProvider {
 
     private val rowsAdapter = ArrayObjectAdapter(ListRowPresenter())
+    private val REQUEST_MICROPHONE_PERMISSION = 1025
+    private lateinit var speechRecognizer: SpeechRecognizer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setSearchResultProvider(this)
         performSearch("")
+
+        if (requestMicrophonePermission()) {
+            startSpeechRecognition()
+        }
+
     }
 
     override fun getResultsAdapter(): ObjectAdapter {
@@ -39,6 +55,7 @@ class SearchFragment : SearchSupportFragment(), SearchSupportFragment.SearchResu
 
     private fun performSearch(query: String) {
         CoroutineScope(Dispatchers.IO).launch {
+            rowsAdapter.clear()
             val searchResults = AllAnimeParser().searchAnime(query)
             withContext(Dispatchers.Main) {
                 val headerItem = HeaderItem("Search Results")
@@ -47,5 +64,76 @@ class SearchFragment : SearchSupportFragment(), SearchSupportFragment.SearchResu
                 rowsAdapter.add(ListRow(headerItem, listRowAdapter))
             }
         }
+    }
+
+    private fun requestMicrophonePermission(): Boolean {
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.RECORD_AUDIO),
+                REQUEST_MICROPHONE_PERMISSION
+            )
+            return false
+        } else {
+            startSpeechRecognition()
+            return true
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_MICROPHONE_PERMISSION) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                startSpeechRecognition()
+            } else {
+                Toast.makeText(requireContext(), "Microphone permission denied", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun startSpeechRecognition() {
+        speechRecognizer =
+            SpeechRecognizer.createSpeechRecognizer(this@SearchFragment.requireContext())
+        speechRecognizer.setRecognitionListener(object : RecognitionListener {
+            override fun onReadyForSpeech(params: Bundle?) {}
+
+            override fun onBeginningOfSpeech() {}
+
+            override fun onRmsChanged(rmsdB: Float) {}
+
+            override fun onBufferReceived(buffer: ByteArray?) {}
+
+            override fun onEndOfSpeech() {}
+
+            override fun onError(error: Int) {}
+
+            override fun onResults(results: Bundle?) {
+                results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.let { matches ->
+                    if (matches.isNotEmpty()) {
+                        val spokenQuery = matches[0]
+                        performSearch(spokenQuery)
+                    }
+                }
+            }
+
+            override fun onPartialResults(partialResults: Bundle?) {}
+
+            override fun onEvent(eventType: Int, params: Bundle?) {}
+        })
+
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
+            putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+        }
+        speechRecognizer.startListening(intent)
     }
 }
