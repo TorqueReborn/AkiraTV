c8cf8e2789b993b18d31084ba1a8e84737292281
TorqueReborn <torquereborn@gmail.com>
2024-09-07 22:55:10 +0530

    Drop everything

--- app/src/main/java/com/ghostreborn/akiratv/MainActivity.kt
+++ app/src/main/java/com/ghostreborn/akiratv/MainActivity.kt
@@ -2,18 +2,10 @@ package com.ghostreborn.akiratv
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
-import com.ghostreborn.akiratv.fragment.SearchFragment
 
 class MainActivity : FragmentActivity() {
-
-    // TODO https://api.github.com/repos/TorqueReborn/AkiraTV/releases/latest
-    //  shows new releases. Scrape when available new update
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main_browse_fragment, SearchFragment())
-            .commitNow()
     }
 }
--- app/src/main/java/com/ghostreborn/akiratv/model/Anime.kt
+++
@@ -1,9 +0,0 @@
-package com.ghostreborn.akiratv.model
-
-import java.io.Serializable
-
-data class Anime(
-    var id: String,
-    val name: String,
-    val thumbnail: String,
-) : Serializable
