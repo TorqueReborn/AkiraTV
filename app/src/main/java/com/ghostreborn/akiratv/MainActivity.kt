81d713647db037e404a20f038b4a78e3a8b14feb
TorqueReborn <torquereborn@gmail.com>
2024-09-08 20:20:37 +0530

    Upgrade AGP, Drop TODO

--- app/src/main/java/com/ghostreborn/akiratv/MainActivity.kt
+++ app/src/main/java/com/ghostreborn/akiratv/MainActivity.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.FragmentActivity
 
 class MainActivity : FragmentActivity() {
 
-    // TODO get created at of assets[0]. If its greater then current (set current time as latest time at first),
-    //  there is an update.
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
