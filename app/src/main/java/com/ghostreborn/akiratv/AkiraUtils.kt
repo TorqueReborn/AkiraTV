8545005a280db69360650f74eef0852e9331ff5e
TorqueReborn <torquereborn@gmail.com>
2024-09-08 07:06:33 +0530

    Open browser if an update is available

--- app/src/main/java/com/ghostreborn/akiratv/AkiraUtils.kt
+++ app/src/main/java/com/ghostreborn/akiratv/AkiraUtils.kt
@@ -1,6 +1,8 @@
 package com.ghostreborn.akiratv
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.widget.Toast
 import androidx.fragment.app.FragmentActivity.MODE_PRIVATE
 import kotlinx.coroutines.CoroutineScope
@@ -8,7 +10,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
-import java.net.HttpURLConnection
 import java.net.URL
 import java.text.SimpleDateFormat
 import java.util.Date
@@ -19,23 +20,20 @@ class AkiraUtils {
 
     fun checkForUpdate(context: Context) {
         CoroutineScope(Dispatchers.IO).launch {
+            val assets = JSONObject(URL("https://api.github.com/repos/TorqueReborn/AkiraTV/releases/latest").readText())
+                .getJSONArray("assets").getJSONObject(0)
             val sp = context.getSharedPreferences(Constants.SHARED_PREF, MODE_PRIVATE)
             val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault()).apply { timeZone = TimeZone.getTimeZone("UTC") }
             val lastUpdate = if (!sp.getBoolean(Constants.PREF_SETUP_COMPLETE, false)) {
-                val conn = URL("https://api.github.com/repos/TorqueReborn/AkiraTV/releases/latest").openConnection() as HttpURLConnection
-                conn.requestMethod = "GET"
-                val response = if (conn.responseCode == HttpURLConnection.HTTP_OK) conn.inputStream.bufferedReader().use { it.readText() } else ""
-                JSONObject(response).getJSONArray("assets").getJSONObject(0).getString("updated_at").also {
+                assets.getString("updated_at").also {
                     sp.edit().putString(Constants.PREF_LAST_DATE, it).putBoolean(Constants.PREF_SETUP_COMPLETE, true).apply()
                 }
             } else sp.getString(Constants.PREF_LAST_DATE, "") ?: ""
-
-            val conn = URL("https://api.github.com/repos/TorqueReborn/AkiraTV/releases/latest").openConnection() as HttpURLConnection
-            conn.requestMethod = "GET"
-            val newTime = sdf.parse(JSONObject(conn.inputStream.bufferedReader().use { it.readText() })
-                .getJSONArray("assets").getJSONObject(0).getString("updated_at")) as Date
+            val newTime = sdf.parse(assets.getString("updated_at")) as Date
             if (newTime.after(sdf.parse(lastUpdate) as Date)) withContext(Dispatchers.Main) {
                 Toast.makeText(context, "Update Available", Toast.LENGTH_SHORT).show()
+                val updateURL = assets.getString("browser_download_url")
+                context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(updateURL)))
             }
         }
     }
