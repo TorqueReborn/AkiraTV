f3679d91b3e7b05d31758c41c0b3d26f65371492
TorqueReborn <torquereborn@gmail.com>
2024-09-10 09:18:44 +0530

    Algo change: Create separate button for update * Clicking button gets latest package

--- app/src/main/java/com/ghostreborn/akiratv/AkiraUtils.kt
+++ app/src/main/java/com/ghostreborn/akiratv/AkiraUtils.kt
@@ -4,35 +4,21 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
-import androidx.fragment.app.FragmentActivity.MODE_PRIVATE
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.json.JSONObject
 import java.net.URL
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
-import java.util.TimeZone
 
 class AkiraUtils {
 
-    fun checkForUpdate(context: Context) {
+    fun getLatestUpdate(context: Context) {
         CoroutineScope(Dispatchers.IO).launch {
-            val assets = JSONObject(URL("https://api.github.com/repos/TorqueReborn/AkiraTV/releases/latest").readText())
-                .getJSONArray("assets").getJSONObject(0)
-            val sp = context.getSharedPreferences(Constants.SHARED_PREF, MODE_PRIVATE)
-            val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault()).apply { timeZone = TimeZone.getTimeZone("UTC") }
-            val lastUpdate = if (!sp.getBoolean(Constants.PREF_SETUP_COMPLETE, false)) {
-                assets.getString("updated_at").also {
-                    sp.edit().putString(Constants.PREF_LAST_DATE, it).putBoolean(Constants.PREF_SETUP_COMPLETE, true).apply()
-                }
-            } else sp.getString(Constants.PREF_LAST_DATE, "") ?: ""
-            val newTime = sdf.parse(assets.getString("updated_at")) as Date
-            if (newTime.after(sdf.parse(lastUpdate) as Date)) withContext(Dispatchers.Main) {
+            val updateURL = JSONObject(URL("https://api.github.com/repos/TorqueReborn/AkiraTV/releases/latest").readText())
+                .getJSONArray("assets").getJSONObject(0).getString("browser_download_url")
+            withContext(Dispatchers.Main) {
                 Toast.makeText(context, "Update Available", Toast.LENGTH_SHORT).show()
-                val updateURL = assets.getString("browser_download_url")
                 context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(updateURL)))
             }
         }
--- app/src/main/java/com/ghostreborn/akiratv/Constants.kt
+++ app/src/main/java/com/ghostreborn/akiratv/Constants.kt
@@ -2,8 +2,4 @@ package com.ghostreborn.akiratv
 
 object Constants {
 
-    val SHARED_PREF = "AKIRA_PREF"
-    val PREF_SETUP_COMPLETE = "SETUP_COMPLETE"
-    val PREF_LAST_DATE = "LAST_DATE"
-
 }
