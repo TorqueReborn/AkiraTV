460c836feb0249d8475adf73d2d744cdf00a1e88
TorqueReborn <torquereborn@gmail.com>
2024-09-10 09:13:49 +0530

    Don't spam github api causes limiting ip

--- app/src/main/java/com/ghostreborn/akiratv/MainActivity.kt
+++ app/src/main/java/com/ghostreborn/akiratv/MainActivity.kt
@@ -8,6 +8,5 @@ class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        AkiraUtils().checkForUpdate(this)
     }
 }
