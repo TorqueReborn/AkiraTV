fed8fcece3d421d5f73d010ec07f39f27863f85f
TorqueReborn <torquereborn@gmail.com>
2025-05-13 21:13:39 +0530

    Add QueryPopular algorithm

--- app/src/main/java/com/ghostreborn/akiratv/MainActivity.kt
+++ app/src/main/java/com/ghostreborn/akiratv/MainActivity.kt
@@ -2,7 +2,7 @@ package com.ghostreborn.akiratv
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
-import com.ghostreborn.akiratv.fragment.SearchFragment
+import com.ghostreborn.akiratv.fragment.TestFragment
 
 class MainActivity : FragmentActivity() {
 
@@ -13,7 +13,7 @@ class MainActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportFragmentManager.beginTransaction()
-            .replace(R.id.main_browse_fragment, SearchFragment())
+            .replace(R.id.main_browse_fragment, TestFragment())
             .commitNow()
     }
 }
---
+++ app/src/main/java/com/ghostreborn/akiratv/fragment/TestFragment.kt
@@ -0,0 +1,36 @@
+package com.ghostreborn.akiratv.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.ghostreborn.akiratv.R
+import com.ghostreborn.akiratv.allAnime.QueryPopular
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class TestFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.test_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val testText: TextView = view.findViewById(R.id.test_text)
+        CoroutineScope(Dispatchers.IO).launch {
+            val test = QueryPopular().queryPopular()
+            withContext(Dispatchers.Main) {
+                testText.text = test.toString()
+            }
+        }
+    }
+
+}
