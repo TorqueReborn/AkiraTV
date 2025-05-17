9ef9db4404a9d3b15d0df5ef7bcdb9ab9bb340cf
TorqueReborn <torquereborn@gmail.com>
2025-05-17 15:39:15 +0530

    Set background image drawable

--- app/src/main/java/com/ghostreborn/akiratv/fragment/MainFragment.kt
+++ app/src/main/java/com/ghostreborn/akiratv/fragment/MainFragment.kt
@@ -1,15 +1,38 @@
 package com.ghostreborn.akiratv.fragment
 
 import android.os.Bundle
+import androidx.leanback.app.BackgroundManager
 import androidx.leanback.app.BrowseSupportFragment
+import com.ghostreborn.akiratv.Utils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainFragment : BrowseSupportFragment() {
+    private lateinit var mBackgroundManager: BackgroundManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mBackgroundManager = BackgroundManager.getInstance(activity)
+        mBackgroundManager.attach(requireActivity().window)
+        updateBackground()
     }
 
     private fun updateBackground() {
+
+        // Make image as drawable
+        CoroutineScope(Dispatchers.IO).launch {
+            val drawable = Utils().getDrawableFromUrl(
+                requireActivity(),
+                "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"
+            )
+            withContext(Dispatchers.Main) {
+                if(drawable != null) {
+                    mBackgroundManager.drawable = drawable
+                }
+            }
+        }
 
     }
 
