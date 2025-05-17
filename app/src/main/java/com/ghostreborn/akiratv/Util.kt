4eb69d04fd86f9bf21902c4838b2ecccbfd22f5f
TorqueReborn <torquereborn@gmail.com>
2025-05-17 15:18:30 +0530

    Drop unwanted piece of code

--- app/src/main/java/com/ghostreborn/akiratv/Util.kt
+++ app/src/main/java/com/ghostreborn/akiratv/Util.kt
@@ -3,8 +3,6 @@ package com.ghostreborn.akiratv
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
-import android.os.Handler
-import android.os.Looper
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
@@ -12,16 +10,6 @@ import com.bumptech.glide.request.transition.Transition
 class Util {
 
     fun urlToBitmap(context: Context, url: String, callback: (Bitmap?) -> Unit) {
-        if (Looper.myLooper() != Looper.getMainLooper()) {
-            Handler(Looper.getMainLooper()).post {
-                startConversion(context, url, callback)
-            }
-        } else {
-            startConversion(context, url, callback)
-        }
-    }
-
-    private fun startConversion(context: Context, url: String, callback: (Bitmap?) -> Unit) {
         Glide.with(context)
             .asBitmap()
             .load(url)
@@ -35,7 +23,6 @@ class Util {
                 override fun onLoadCleared(placeholder: Drawable?) {
                     callback(null)
                 }
-
             })
     }
 
