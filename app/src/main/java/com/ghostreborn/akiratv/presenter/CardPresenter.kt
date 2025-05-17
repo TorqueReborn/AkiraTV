fda6a348b6e6d250ce281d59d0bb9c7d1963ff3c
TorqueReborn <torquereborn@gmail.com>
2025-05-17 13:23:49 +0530

    Drop Anime Name Text

--- app/src/main/java/com/ghostreborn/akiratv/presenter/CardPresenter.kt
+++ app/src/main/java/com/ghostreborn/akiratv/presenter/CardPresenter.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.leanback.widget.Presenter
 import coil.load
 import com.ghostreborn.akiratv.R
