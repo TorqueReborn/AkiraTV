c23f19925ab2b14e41e227b71eb6d15fb7d0bae2
TorqueReborn <torquereborn@gmail.com>
2025-05-17 10:22:18 +0530

    Drop Everything. Major change in code

--- app/src/main/java/com/ghostreborn/akiratv/MainActivity.kt
+++ app/src/main/java/com/ghostreborn/akiratv/MainActivity.kt
@@ -2,15 +2,11 @@ package com.ghostreborn.akiratv
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
-import com.ghostreborn.akiratv.fragment.HomeFragment
 
 class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main_browse_fragment, HomeFragment())
-            .commitNow()
     }
 }
--- app/src/main/java/com/ghostreborn/akiratv/model/Anime.kt
+++
@@ -1,7 +0,0 @@
-package com.ghostreborn.akiratv.model
-
-data class Anime(
-    var id: String,
-    val name: String,
-    val thumbnail: String
-)
