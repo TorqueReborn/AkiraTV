9bcdb9c9e1f87b5ff92dcf3a0bda0948943fbb6e
TorqueReborn <torquereborn@gmail.com>
2025-05-17 13:30:25 +0530

    Test Background Drawable

--- app/src/main/java/com/ghostreborn/akiratv/fragment/MainFragment.kt
+++ app/src/main/java/com/ghostreborn/akiratv/fragment/MainFragment.kt
@@ -1,51 +1,40 @@
 package com.ghostreborn.akiratv.fragment
 
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.ColorFilter
+import android.graphics.Paint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import androidx.leanback.app.BackgroundManager
 import androidx.leanback.app.BrowseSupportFragment
-import androidx.leanback.widget.ArrayObjectAdapter
-import androidx.leanback.widget.HeaderItem
-import androidx.leanback.widget.ListRow
-import androidx.leanback.widget.ListRowPresenter
-import com.ghostreborn.akiratv.model.Anime
-import com.ghostreborn.akiratv.presenter.CardPresenter
 
 class MainFragment : BrowseSupportFragment() {
+
+    private lateinit var mBackgroundManager: BackgroundManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         headersState = HEADERS_DISABLED
+        mBackgroundManager = BackgroundManager.getInstance(activity)
+        mBackgroundManager.attach(requireActivity().window)
 
-        val rowsAdapter = ArrayObjectAdapter(ListRowPresenter())
-        val cardPresenter = CardPresenter()
+        updateBackground()
+    }
 
-        val listRowAdapter = ArrayObjectAdapter(cardPresenter)
-        listRowAdapter.add(
-            Anime(
-                "One Piece",
-                "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"
-            )
-        )
-        listRowAdapter.add(
-            Anime(
-                "One Piece",
-                "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"
-            )
-        )
-        listRowAdapter.add(
-            Anime(
-                "One Piece",
-                "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"
-            )
-        )
-        listRowAdapter.add(
-            Anime(
-                "One Piece",
-                "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"
-            )
-        )
-        val header = HeaderItem(0, "Anime")
-        rowsAdapter.add(ListRow(header, listRowAdapter))
-        adapter = rowsAdapter
+    private fun updateBackground() {
+        val drawable = object: Drawable() {
+            override fun draw(canvas: Canvas) {
+                val paint = Paint()
+                paint.color = Color.WHITE
+                paint.style = Paint.Style.FILL
+                canvas.drawRect(0f, 0f, bounds.width().toFloat(), bounds.height().toFloat(), paint)
+            }
+            override fun setAlpha(alpha: Int) {}
+            override fun setColorFilter(colorFilter: ColorFilter?) {}
+            override fun getOpacity(): Int = android.graphics.PixelFormat.TRANSLUCENT
+        }
+        mBackgroundManager.drawable = drawable
     }
 
 }
