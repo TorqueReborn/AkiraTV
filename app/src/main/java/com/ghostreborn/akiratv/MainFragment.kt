1f6f9d1d653137745ef86d9df84053e1b802aad0
TorqueReborn <torquereborn@gmail.com>
2025-05-17 13:04:56 +0530

    Disable Headers

--- app/src/main/java/com/ghostreborn/akiratv/MainFragment.kt
+++ app/src/main/java/com/ghostreborn/akiratv/MainFragment.kt
@@ -1,21 +1,15 @@
 package com.ghostreborn.akiratv
 
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.leanback.app.BrowseSupportFragment
 import androidx.leanback.widget.ArrayObjectAdapter
 import androidx.leanback.widget.HeaderItem
 import androidx.leanback.widget.ListRow
 import androidx.leanback.widget.ListRowPresenter
-import androidx.leanback.widget.Presenter
-import androidx.leanback.widget.PresenterSelector
-import androidx.leanback.widget.RowHeaderPresenter
 import com.ghostreborn.akiratv.model.Anime
 import com.ghostreborn.akiratv.presenter.CardPresenter
 
-class MainFragment: BrowseSupportFragment() {
+class MainFragment : BrowseSupportFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +19,30 @@ class MainFragment: BrowseSupportFragment() {
         val cardPresenter = CardPresenter()
 
         val listRowAdapter = ArrayObjectAdapter(cardPresenter)
-        listRowAdapter.add(Anime("One Piece", "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"))
-        listRowAdapter.add(Anime("One Piece", "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"))
-        listRowAdapter.add(Anime("One Piece", "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"))
-        listRowAdapter.add(Anime("One Piece", "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"))
+        listRowAdapter.add(
+            Anime(
+                "One Piece",
+                "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"
+            )
+        )
+        listRowAdapter.add(
+            Anime(
+                "One Piece",
+                "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"
+            )
+        )
+        listRowAdapter.add(
+            Anime(
+                "One Piece",
+                "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"
+            )
+        )
+        listRowAdapter.add(
+            Anime(
+                "One Piece",
+                "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"
+            )
+        )
         val header = HeaderItem(0, "Anime")
         rowsAdapter.add(ListRow(header, listRowAdapter))
         adapter = rowsAdapter
