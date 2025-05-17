c3a307b6c3634ed15125866073c8876ff4e97c84
TorqueReborn <torquereborn@gmail.com>
2025-05-17 13:04:34 +0530

    Disable Headers

--- app/src/main/java/com/ghostreborn/akiratv/MainFragment.kt
+++ app/src/main/java/com/ghostreborn/akiratv/MainFragment.kt
@@ -2,31 +2,25 @@ package com.ghostreborn.akiratv
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.leanback.app.BrowseSupportFragment
 import androidx.leanback.widget.ArrayObjectAdapter
 import androidx.leanback.widget.HeaderItem
 import androidx.leanback.widget.ListRow
 import androidx.leanback.widget.ListRowPresenter
+import androidx.leanback.widget.Presenter
+import androidx.leanback.widget.PresenterSelector
+import androidx.leanback.widget.RowHeaderPresenter
 import com.ghostreborn.akiratv.model.Anime
 import com.ghostreborn.akiratv.presenter.CardPresenter
 
 class MainFragment: BrowseSupportFragment() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        headersState = HEADERS_DISABLED
 
-        setupUI()
-        loadRows()
-    }
-
-    private fun setupUI() {
-        headersState = HEADERS_HIDDEN
-        isHeadersTransitionOnBackEnabled = true
-        brandColor = ContextCompat.getColor(requireActivity(), R.color.brand_color)
-    }
-
-    private fun loadRows() {
         val rowsAdapter = ArrayObjectAdapter(ListRowPresenter())
         val cardPresenter = CardPresenter()
 
@@ -35,7 +29,6 @@ class MainFragment: BrowseSupportFragment() {
         listRowAdapter.add(Anime("One Piece", "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"))
         listRowAdapter.add(Anime("One Piece", "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"))
         listRowAdapter.add(Anime("One Piece", "https://wp.youtube-anime.com/s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx21-YCDoj1EkAxFn.jpg?w=250"))
-
         val header = HeaderItem(0, "Anime")
         rowsAdapter.add(ListRow(header, listRowAdapter))
         adapter = rowsAdapter
